0f289936586e321aad15fa1b07e6980f401c946f
Max <myugay@beeline.kg>
2019-11-27 13:15:26 +0600

    remove wrong line from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ fi
 
 if [[ ! -f /usr/share/nginx/certificates/cert.crt ]]; then
     openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
-    sl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
     openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
     "/C=PT/ST=World/L=World/O=${DOMAIN:-ilhicas.com}/OU=Ilhicas/CN=${DOMAIN:-ilhicas.com}/EMAIL=${EMAIL:-info@ilhicas.com}"
     openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
